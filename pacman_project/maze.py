d0d4a89e39818c8ce74a568faa316be9fefd4017
Andrew Lau <andrewlau2019@gmail.com>
2022-10-22 02:57:45 -0700

    Maze

--- pacman_project/maze.py
+++ pacman_project/maze.py
@@ -1,14 +1,14 @@
-import pygame
+import pygame as pg
 
 # list of nodes
-maze = [
-    "|--------|",
-    "|G..|..G.|",
-    "|...PP...|",
-    "|G...@.|.|",
-    "|........|",
-    "|--------|"
-]
+# maze = [
+#     "|--------|",
+#     "|G..|..G.|",
+#     "|...PP...|",
+#     "|G...@.|.|",
+#     "|........|",
+#     "|--------|"
+# ]
 
 # 01111111111111011111111111110
 # 01000010000001010000010000010
@@ -38,9 +38,42 @@ maze = [
 # 01000000000010100000000000010
 # 01111111111111111111111111110
 
+
 class Maze:
-    def __int__(self):
+    def __init__(self, screen, settings, mazefile):
         self.pacman_pos = (200, 400)
+        self.screen = screen
+        self.settings = settings
+        self.filename = mazefile
+        with open(self.filename, 'r') as f:
+            self.rows = f.readlines()
+
+        self.bricks = []
+        self.brick = pg.image.load('images/Block.png')
+        self.brick = pg.transform.scale(self.brick, (16, 16))
+        self.rect = self.brick.get_rect()
+
+        self.build()
+
+    def build(self):
+        b = self.brick.get_rect()
+        bw, bh = b.width, b.height
+
+        for nrow in range(len(self.rows)):
+            row = self.rows[nrow]
+            for ncol in range(len(row)):
+                col = row[ncol]
+                if col == 'X':
+                    self.bricks.append(pg.Rect(ncol, nrow, bw, bh))
+    def wall_collide(self): pass
+
+
+    def draw(self):
+        for rect in self.bricks:
+            self.screen.blit(self.brick, rect)
+
+    def update(self):
+        self.draw()
 
 
 
